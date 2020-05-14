386bd0081115a2536f420a753aed04334a71c9fd
Romi <rahul.hans@outlook.com>
2020-05-14 18:23:57 +0530

    another update

--- src/test/java/cucumberframework/runner/AppiumAPKTestRunner.java
+++ src/test/java/cucumberframework/runner/AppiumAPKTestRunner.java
@@ -4,6 +4,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 
+/* added a comment */
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
          features = {"src/test/resources/features"},
